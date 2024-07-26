d6d03558403661df6c02ed306da9faa29e1818b2
Yash Dhiraj Oza <144452060+YashDhirajOza@users.noreply.github.com>
2024-07-26 13:38:56 +0530

    Update server.c

--- server.c
+++ server.c
@@ -7,23 +7,22 @@
 #include <netinet/in.h>
 #include <unistd.h>
 
-// Define constants
 #define SERVER_CERT "server.crt"
 #define SERVER_KEY "server.key"
 #define SERVER_PORT 443
 
-// Function to handle client connections
+// to handle client connections
 void handle(SSL *ssl) {
-    // Define HTTP response
+    // HTTP response
     char res[] = "HTTP/1.1 200 OK\r\n"
                  "Content-Type: text/html\r\n"
                  "\r\n"
                  "<html><body><h1>HTTPS SERVER ON CRACK</h1></body></html>\r\n";
     
-    // Send response over SSL connection
+    // send response over SSL connection
     SSL_write(ssl, res, strlen(res));
     
-    // Clean up SSL connection
+    // clean up SSL connection
     SSL_shutdown(ssl);
     SSL_free(ssl);
 }
@@ -35,12 +34,12 @@ int main() {
     socklen_t addr_len = sizeof(client_addr);
     SSL *ssl;
 
-    // Initialize OpenSSL
+    //  OpenSSL
     SSL_library_init();
     SSL_load_error_strings();
     OpenSSL_add_ssl_algorithms();
 
-    // Create a new SSL context
+    // new SSL context
     ctx = SSL_CTX_new(SSLv23_server_method());
     if (!ctx) {
         perror("Unable to create SSL context");
@@ -48,32 +47,32 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    // Load the server certificate
+    //  the server certificate
     if (SSL_CTX_use_certificate_file(ctx, SERVER_CERT, SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
-    // Load the private key
+    // private key
     if (SSL_CTX_use_PrivateKey_file(ctx, SERVER_KEY, SSL_FILETYPE_PEM) <= 0) {
         ERR_print_errors_fp(stderr);
         exit(EXIT_FAILURE);
     }
 
-    // Create a socket
+    // make a socket
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server_fd < 0) {
         perror("Unable to create socket");
         exit(EXIT_FAILURE);
     }
 
-    // Configure server address
+    //  server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(SERVER_PORT);
 
-    // Bind the socket to the server address
+
     if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
         perror("Unable to bind");
         exit(EXIT_FAILURE);
@@ -87,32 +86,32 @@ int main() {
 
     printf("Server listening on port %d\n", SERVER_PORT);
 
-    // Main server loop
+   
     while(1) {
-        // Accept a client connection
+    
         client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &addr_len);
         if (client_fd < 0) {
             perror("Unable to accept");
             continue;
         }
 
-        // Create a new SSL structure for this connection
+    
         ssl = SSL_new(ctx);
         SSL_set_fd(ssl, client_fd);
 
-        // Perform SSL handshake
+       
         if (SSL_accept(ssl) <= 0) {
             ERR_print_errors_fp(stderr);
         } else {
-            // Handle the client request
+     
             handle(ssl);
         }
 
-        // Close the client socket
+    
         close(client_fd);
     }
 
-    // Clean up (Note: This part is never reached in this infinite loop)
+  
     SSL_CTX_free(ctx);
     close(server_fd);
     return 0;
